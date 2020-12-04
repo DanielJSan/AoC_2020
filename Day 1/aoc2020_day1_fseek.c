cb107c8c30729c1b3503941c5979b35a05c3f21f
DanielJSan <danieljsan@usal.es>
2020-12-04 23:53:52 +0100

    Fixed Day 1 optimized version

--- Day 1/aoc2020_day1_fseek.c
+++ Day 1/aoc2020_day1_fseek.c
@@ -9,57 +9,52 @@ int main() {
 
 	FILE *pFile = fopen("input.txt", "r");
 	
+	if(pFile == NULL) {
+	
+		fprintf(stderr, "Null file\n");
+		return 1;
+	
+	}
+	
 	fseek(pFile, 0, SEEK_END);
 	int tam = ftell(pFile);
 	rewind(pFile);
 	
-	char input[800];
+	char input[tam];
 	int num[200];
 	
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int exit = 0;
 	
-	//printf("tam = %d\n", tam);
-	
-	int i, j, k, exit= 0;
-	
+	printf("Input data table: \n");
 	while (fscanf(pFile, "%s", input) != EOF) { //Reading input.txt file
-	
-		printf("%s\n", input);
+		
+		if(j == 10) {
+		
+			printf("|\n");
+			j = 0;
+		}
+		
+		printf("| %s\t ", input);
+		
 		num[i] = atoi(input); //Char to int parse
 		
 		i++;
+		j++;
 		
 	}
 	
+	//fprintf(stderr, "File scanned succesfully\n");
+	printf("|\n\n");
 	
-	/*
-	while (fgets(input, 200 + 1, pFile) != 0) { //Reading input.txt file
-	
-		printf("%s\n", input);
-		num[i] = atoi(input); //Char to int parse
-		i++;
-		
-	}
-	*/
-	
-	/*
-	char* dump = (char *)malloc(sizeof(char) * tam);
-	
-	while (fscanf(pFile, "%s", input) != EOF) {
-		printf("%s\n", input);
-		dump[i] = atoi(input); //Char to int parse
-		i++;
-	}
-	*/
-	
-	
-
 	for(i = 0; i < 199; i++) { //First star
 	
 		for(j = 1; j < 200; j++) {
 		
 			if(num[i] + num[j] == 2020) {
 				
-				printf("\n%d * %d = ", num[i], num[j]);
 				printf("Star 1 flag: %d\n", num[i] * num[j]);
 				exit = 1;
 			}
@@ -81,15 +76,14 @@ int main() {
 			for(k = 2; k < 200; k++) {
 		
 				if(num[i] + num[j] + num[k] == 2020) {
-					
-					printf("\n%d * %d * %d = ", num[i], num[j], num[k]);
+
 					printf("Star 2 flag: %d\n", num[i] * num[j] * num[k]);
 					return 1;
 				}
 			}
 		}
 	}
-
+	
 	fclose(pFile);
 	
 }
